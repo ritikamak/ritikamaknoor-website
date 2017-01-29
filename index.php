6571203977e6a4691ba303ca464f8eb43486c760
ritikamak <rim32@pitt.edu>
2017-01-29 17:45:44 -0500

    no about sec

--- index.php
+++ index.php
@@ -77,63 +77,7 @@
 
 
 
-    <!-- About Section -->
-    <div class="container">
-        <div class="about" id="about">
-            <h2 class="featurette-heading">About Me
-                <span class="text-muted"></span>
-            </h2>
-            <p class="lead">Hi there! I'm Ritika Maknoor. 
-            I'm a web designer and software developer based in Pittsburgh/NYC. I am currently completing my final year at the University of Pittsburgh- working towards obtaining my B.S. in Computer Science.</p>
-        </div>
-    </div>
 
-    
-
-
-
-
-
-
-
-
-    <div class="container">
-        <div class="photos">
-            <div class="row">
-                <div class="box">
-                    <div class="col-lg-12 text-center">
-                        <div id="carousel-example-generic" class="carousel slide">
-                            <!-- Indicators -->
-                            <ol class="carousel-indicators hidden-xs">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <!-- Wrapper for slides -->
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="img-responsive img-full" src="img/img1.jpg" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="img-responsive img-full" src="img/img2.jpg" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="img-responsive img-full" src="img/img3.jpg" alt="">
-                                </div>
-                            </div>
-                            <!-- Controls -->
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="icon-prev"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="icon-next"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
 
