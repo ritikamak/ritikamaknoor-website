4ba1d6fbf9585129b601c6c2546d1f965cf65449
ritikamak <rim32@pitt.edu>
2017-02-27 19:07:10 -0500

    added php form redirect

--- contactsection.php
+++ contactsection.php
@@ -80,13 +80,90 @@
         </div>
     </section> 
 
+
+
+
+
+
+
+
     <!-- FORM -->
-    <form id="form" class="topBefore">
-              <input id="name" type="text" placeholder="NAME">
-              <input id="email" type="text" placeholder="E-MAIL">
-              <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+    <form id="form" class="topBefore" method="post" name="myemailform" action="form-to-email">
+              <input id="name" type="text" placeholder="NAME" name="name">
+              <input id="email" type="text" placeholder="E-MAIL" name="email">
+              <textarea id="message" type="text" placeholder="MESSAGE" name="message"></textarea>
       <input id="submit" type="submit" value="SUBMIT">
     </form>
+<?php
+if(!isset($_POST['submit']))
+{
+    //This page should not be accessed directly. Need to submit the form.
+    echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'contact@ritikamaknoor.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+    
+$to = "contact@ritikamaknoor.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
+
+
+
+
+
+
+
+
+
 
     <!-- About Me Writing -->
     <section id="contactbuttons-writing" class="contactbuttons-writing">
